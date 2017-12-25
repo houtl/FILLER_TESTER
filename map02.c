7f63edb07f6108031631ed656f8653fd4a5ee00d
thou <houtianle@gmail.com>
2017-12-25 17:50:00 +0100

    Update map02.c

--- map02.c
+++ map02.c
@@ -45,6 +45,7 @@ void	map02p1(t_prm *prm, char player[21])
 			read(prm->fd, prm->buf, 4096);
 			if (strstr(prm->buf, player) && !strstr(prm->buf, "Segfault"))
 				score++;
+			printf("等待。。。");
 			j++;
 		}
 		print_result(player, prm->player_lst[i], prm->nb1, score);
